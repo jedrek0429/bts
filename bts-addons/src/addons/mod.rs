b99de298674852a2ed84d5636c391292f07865d4
Andrzej Bansleben <a.bansleben0429@gmail.com>
2026-07-29 16:17:11 +0200

    Coordinate addon handlers

--- bts-addons/src/addons/mod.rs
+++ bts-addons/src/addons/mod.rs
@@ -2,7 +2,34 @@ pub(crate) mod clock;
 pub(crate) mod message;
 pub(crate) mod weather;
 
-pub(crate) fn requested_digit(event: &bts_protocol::Event) -> Option<&str> {
+use anyhow::Result;
+use bts_protocol::Event;
+
+use crate::AddonContext;
+
+pub(crate) struct Addons {
+    clock: clock::ClockAddon,
+    weather: weather::WeatherAddon,
+}
+
+impl Addons {
+    pub(crate) fn new() -> Self {
+        Self {
+            clock: clock::ClockAddon::new(),
+            weather: weather::WeatherAddon::new(),
+        }
+    }
+
+    pub(crate) async fn handle(&self, context: &AddonContext, event: &Event) -> Result<()> {
+        match requested_digit(event) {
+            Some(clock::DIGIT) => self.clock.handle(context, event).await,
+            Some(weather::DIGIT) => self.weather.handle(context, event).await,
+            _ => Ok(()),
+        }
+    }
+}
+
+pub(crate) fn requested_digit(event: &Event) -> Option<&str> {
     match &event.kind {
         bts_protocol::EventKind::PhoneDtmfReceived { digit, .. } => Some(digit.as_str()),
         _ => None,
